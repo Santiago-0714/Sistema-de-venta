6a4b9af8df01969e029c1a0a4ef92083e634b021
Admin <Admin@10.62.0.137>
2022-10-18 17:08:24 -0500

    Ventana de login creada

---
+++ src/sistemaventas/SistemaVentas.java
@@ -0,0 +1,12 @@
+
+package sistemaventas;
+/**
+ *
+ * @author Santiago Carvajal Lara
+ */
+public class SistemaVentas {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
